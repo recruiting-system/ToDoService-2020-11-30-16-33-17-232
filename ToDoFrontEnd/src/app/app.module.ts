84062e6e474692fb77deb7f1533a99c042e609d5
linwenjun <linwenjun@163.com>
2020-11-30 16:33:34 +0000

    first commit

---
+++ ToDoFrontEnd/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateTodoitemComponent } from './create-todoitem/create-todoitem.component';
+import { ListTodoitemComponent } from './list-todoitem/list-todoitem.component';
+import { TodoitemDetailComponent } from './todoitem-detail/todoitem-detail.component';
+import { FormsModule } from '@angular/forms';
+import { UpdateTodoItemComponent } from './update-todo-item/update-todo-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateTodoitemComponent,
+    ListTodoitemComponent,
+    TodoitemDetailComponent,
+    UpdateTodoItemComponent,    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
